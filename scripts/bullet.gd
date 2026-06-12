19c90458171515085ad6f48b162f517bb6da6d20
Kenton <38125586+Kenton1989@users.noreply.github.com>
2026-06-13 01:45:04 +0800

    fix bullet go across walls

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -15,13 +15,21 @@ func setup(init_direction: Vector2) -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if _inside_world_body():
+		queue_free()
+		return
+
 	remaining_lifetime = max_lifetime
 	area_entered.connect(_on_area_entered)
 
 func _physics_process(delta: float) -> void:
+	if _inside_world_body():
+		queue_free()
+		return
+
 	var current_position := global_position
 	var next_position := current_position + direction * speed * delta
-	
+
 	if _will_hit_world(current_position, next_position):
 		queue_free()
 		return
@@ -40,11 +48,11 @@ func _on_area_entered(area: Area2D):
 	queue_free()
 
 func _will_hit_world(from_pos: Vector2, to_pos: Vector2):
-	var space_state = get_world_2d().direct_space_state
+	var space_state := get_world_2d().direct_space_state
 	if space_state == null:
 		return false
-	
-	var query = PhysicsRayQueryParameters2D.create(
+
+	var query := PhysicsRayQueryParameters2D.create(
 		from_pos,
 		to_pos,
 		WORLD_COLLISION_MASK
@@ -54,4 +62,15 @@ func _will_hit_world(from_pos: Vector2, to_pos: Vector2):
 	
 	var collision = space_state.intersect_ray(query)
 	return not collision.is_empty()
+
+func _inside_world_body():
+	var space_state := get_world_2d().direct_space_state
+	if space_state == null:
+		return false
+
+	var query := PhysicsPointQueryParameters2D.new()
+	query.collision_mask = WORLD_COLLISION_MASK
+	query.position = position
 	
+	var collision = space_state.intersect_point(query)
+	return not collision.is_empty()
